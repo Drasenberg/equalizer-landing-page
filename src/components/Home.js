9ee656e1bc105e0811fdb5092193a515cf1a7fec
Drasenberg <macoel14@gmail.com>
2022-01-16 22:24:49 +0100

    Working on Main Content of Site

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,17 +16,25 @@ function Home() {
           parametric EQ & volume mixer. Control bass, mids, treble, gain
           control, reverb, and more!
         </p>
-        <h2>Premium EQ</h2>
-        <p>
-          Get expert-level control with a robust equalizer, volume mixer, and
-          spatial audio. Take your listening experience to a whole new level and
-          access all our incredible features!
-        </p>
-        <p>
-          <span>$4</span> / month{" "}
-        </p>
-        <button>iOS Download</button>
-        <button>Android Download</button>
+        <div className={classes.appDesc}>
+          <div className={classes.bgContainer}>
+            <div className={classes.bgImage}></div>
+            <div className={classes.appImage}></div>
+          </div>
+          <div className={classes.contentDesc}>
+            <h2>Premium EQ</h2>
+            <p>
+              Get expert-level control with a robust equalizer, volume mixer,
+              and spatial audio. Take your listening experience to a whole new
+              level and access all our incredible features!
+            </p>
+            <p>
+              <span>$4</span> / month{" "}
+            </p>
+            <button>iOS Download</button>
+            <button>Android Download</button>
+          </div>
+        </div>
       </main>
 
       <footer>
