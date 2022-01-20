87e3f1450e30ed1d27b33e6a02c0311a27ad9de8
Drasenberg <macoel14@gmail.com>
2022-01-20 02:57:36 +0100

    App Finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,25 +2,26 @@ import React from "react";
 import classes from "../styles/Home.module.scss";
 import iconApple from "../assets/icon-apple.svg";
 import iconAndroid from "../assets/icon-android.svg";
-import iconFacebook from "../assets/icon-facebook.svg";
-import iconInstagram from "../assets/icon-instagram.svg";
-import iconTwitter from "../assets/icon-twitter.svg";
 
 function Home() {
   return (
     <div className={classes.container}>
-      <div className={classes.background}></div>
+      <div className={classes.background}>
+        <div className={classes.innerBg}></div>
+      </div>
       <header>
         <div className={classes.logo}></div>
       </header>
       <main>
-        <h1>We make your music sound extraordinary.</h1>
-        <p>
-          A system audio equalizer specifically designed for Android and iOS.
-          Freely tune the way your music sounds with a professional grade
-          parametric EQ & volume mixer. Control bass, mids, treble, gain
-          control, reverb, and more!
-        </p>
+        <div className={classes.intro}>
+          <h1>We make your music sound extraordinary.</h1>
+          <p>
+            A system audio equalizer specifically designed for Android and iOS.
+            Freely tune the way your music sounds with a professional grade
+            parametric EQ & volume mixer. Control bass, mids, treble, gain
+            control, reverb, and more!
+          </p>
+        </div>
         <div className={classes.appDesc}>
           <div className={classes.bgContainer}>
             <div className={classes.bgImage}></div>
@@ -38,11 +39,12 @@ function Home() {
             </p>
             <div className={classes.controls}>
               <button className={classes.ios}>
-                <img src={iconApple} />
+                <img src={iconApple} alt="ios download" />
                 iOS Download
               </button>
               <button className={classes.android}>
-                <img src={iconAndroid} /> Android Download
+                <img src={iconAndroid} alt="android download" /> Android
+                Download
               </button>
             </div>
           </div>
@@ -50,21 +52,17 @@ function Home() {
       </main>
 
       <footer>
-        <div className={classes.logo}></div>
         <div className={classes.footerContent}>
-          All rights reserved © Equalizer 2021 Have any problems? Contact us via
-          social media or email us at <br /> <span>equalizer@example.com</span>
+          <div className={classes.logo}></div>
+          <div className={classes.text}>
+            All rights reserved © Equalizer 2021 Have any problems? Contact us
+            via social media or email us at <span>equalizer@example.com</span>
+          </div>
         </div>
         <div className={classes.social}>
-          <button>
-            <img src={iconFacebook} alt="facebook" />
-          </button>
-          <button>
-            <img src={iconInstagram} alt="instagram" />
-          </button>
-          <button>
-            <img src={iconTwitter} alt="twitter" />
-          </button>
+          <button className={classes.facebook}></button>
+          <button className={classes.instagram}></button>
+          <button className={classes.twitter}></button>
         </div>
       </footer>
     </div>
